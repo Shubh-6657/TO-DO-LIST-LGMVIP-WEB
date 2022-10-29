294d167fc1893c1c20cf4e163d82d9eb35b81c9c
Shubh-6657 <108170423+Shubh-6657@users.noreply.github.com>
2022-10-29 09:41:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+const taskInput = document.querySelector(".task-input input"),
+taskBox = document.querySelector(".task-box");
+let todos =JSON.parse(localStorage.getItem("todo-list"));
+
+function showTodo(){
+    let li = "";
+    todos.forEach((todo, id) => {
+        li += `<li class="task">
+                <label for="${id}">
+                    <input type="checkbox" id="${id}">
+                    <p>${todo.name}</p>
+                </label>
+                <div class="settings">
+                    <i class="uil uil-ellipsis-h"></i>
+                    <ul class="task-menu">
+                        <li><i class="uil uil-pen"></i>Edit
+                        </li>
+                        <li><i class="uil uil-trash"></i>Delete
+                        </li>
+                    </ul>
+                </div>
+            </li>`;
+    });
+    taskBox.innerHTML = li;
+}  
+showTodo();
+
+taskInput.addEventListener("keyup", e =>{
+    let userTask = taskInput.value.trim();
+    if(e.key == "Enter" && userTask) {
+        if(!todos){
+            todos=[];
+        }
+        taskInput.value = "";
+        let taskInfo = {name:userTask, status:"pending"};
+        todos.push(taskInfo);
+        localStorage.setItem("todo-list",JSON.stringify(todos));
+        showTodo();
+    }
+});
